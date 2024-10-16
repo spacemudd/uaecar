f97dd9616d6f6ed56b7bc1aeb34e24b84018e96e
Abdelrahman Yousef <abdelrahman.yousef@hadaf-hq.com>
2024-10-16 08:37:40 +0300

    update car_Table

--- database/migrations/2024_10_11_153340_create_cars_table.php
+++ database/migrations/2024_10_11_153340_create_cars_table.php
@@ -20,8 +20,22 @@ return new class extends Migration
             $table->string('car_picture');
             $table->integer('doors');
             $table->boolean('air_condition')->default(false);
-            $table->integer('age');
             $table->text('description')->nullable();
+            $table->string('plate_number')->nullable();
+            $table->string('make')->nullable();
+            $table->string('model')->nullable();
+            $table->year('year')->nullable();
+            $table->string('color')->nullable();
+            $table->string('categories')->nullable();
+            $table->string('current_location')->nullable();
+            $table->string('status')->nullable();
+            $table->date('expected_return_date')->nullable();
+            $table->string('upcoming_reservation')->nullable();
+            $table->date('latest_return_date')->nullable();
+            $table->integer('odometer')->nullable();
+            $table->string('chassis_number')->nullable();
+            $table->date('license_expiry_date')->nullable();
+            $table->date('insurance_expiry_date')->nullable();
             $table->timestamps();
         });
     }
