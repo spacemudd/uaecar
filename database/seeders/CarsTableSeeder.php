9d917f1d67561ad781034d1bf5b341ea3f3c8983
Abdelrahman Yousef <abdelrahman.yousef@hadaf-hq.com>
2024-10-16 11:40:12 +0300

    update data in cars

--- database/seeders/CarsTableSeeder.php
+++ database/seeders/CarsTableSeeder.php
@@ -15,36 +15,36 @@ class CarsTableSeeder extends Seeder
     public function run()
     {
 
-        Car::where('plate_number', 'A-52271')->update([
-            'car_picture' => 'front/img/cars/HondaOdyssey2020_1.png'
-        ]);
+        // Car::where('plate_number', 'A-52271')->update([
+        //     'car_picture' => 'front/img/cars/HondaOdyssey2020_1.png'
+        // ]);
 
-        Car::where('plate_number', 'B-48806')->update([
-            'car_picture' => 'front/img/cars/nissansunny1.png'
-        ]);
+        // Car::where('plate_number', 'B-48806')->update([
+        //     'car_picture' => 'front/img/cars/nissansunny1.png'
+        // ]);
         
-        Car::where('plate_number', 'B-69415')->update([
-            'car_picture' => 'front/img/cars/Hyundai Sonata.png'
-        ]);
+        // Car::where('plate_number', 'B-69415')->update([
+        //     'car_picture' => 'front/img/cars/Hyundai Sonata.png'
+        // ]);
 
-        Car::where('plate_number', 'A-50943')->update([
-            'car_picture' => 'front/img/cars/Santa_Fe_Hybrid.png'
-        ]);
+        // Car::where('plate_number', 'A-50943')->update([
+        //     'car_picture' => 'front/img/cars/Santa_Fe_Hybrid.png'
+        // ]);
 
-        Car::where('plate_number', 'A-52240')->update([
-            'car_picture' => 'front/img/cars/mitsubishi_outlander_-small-15752.png'
-        ]);
+        // Car::where('plate_number', 'A-52240')->update([
+        //     'car_picture' => 'front/img/cars/mitsubishi_outlander_-small-15752.png'
+        // ]);
 
-        Car::where('plate_number', 'A-52270')->update([
-            'car_picture' => 'front/img/cars/bmw228i.png'
-        ]);
+        // Car::where('plate_number', 'A-52270')->update([
+        //     'car_picture' => 'front/img/cars/bmw228i.png'
+        // ]);
 
-        Car::where('plate_number', 'A-53169')->update([
-            'car_picture' => 'front/img/cars/Hyundai Venue.png'
-        ]);
+        // Car::where('plate_number', 'A-53169')->update([
+        //     'car_picture' => 'front/img/cars/Hyundai Venue.png'
+        // ]);
 
          // Delete rows with IDs from 20 to 30
-         Car::whereBetween('id', [20, 30])->delete();
+         Car::where('id', [18, 17])->delete();
           
     
 }
