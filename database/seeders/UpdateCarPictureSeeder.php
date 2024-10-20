340b2ce38aebe9950bd0d1d5969d0615ef8fcd2a
Abdelrahman Yousef <abdelrahman.yousef@hadaf-hq.com>
2024-10-20 10:17:47 +0300

    change the car_price type column

--- database/seeders/UpdateCarPictureSeeder.php
+++ database/seeders/UpdateCarPictureSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
 
 class UpdateCarPictureSeeder extends Seeder
 {
@@ -12,66 +14,73 @@ class UpdateCarPictureSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('cars')
-        ->where('id', 4)
-        ->update(['car_picture' => 'front/img/cars/nissansunny2020.jpg']);
+
+        Schema::table('cars', function (Blueprint $table) {
+            // Change the column type to integer
+            $table->integer('price_daily')->change();
+        });
 
 
-        DB::table('cars')
-        ->where('id', 6)
-        ->update(['car_picture' => 'front/img/cars/hyundai-santa-fe-2023-exterior-oem-01.jpg']);
+        // DB::table('cars')
+        // ->where('id', 4)
+        // ->update(['car_picture' => 'front/img/cars/nissansunny2020.jpg']);
+
+
+        // DB::table('cars')
+        // ->where('id', 6)
+        // ->update(['car_picture' => 'front/img/cars/hyundai-santa-fe-2023-exterior-oem-01.jpg']);
 
         
-        DB::table('cars')
-        ->where('id', 9)
-        ->update(['car_picture' => 'front/img/cars/kiacerato2020.jpg']);
+        // DB::table('cars')
+        // ->where('id', 9)
+        // ->update(['car_picture' => 'front/img/cars/kiacerato2020.jpg']);
 
 
-        DB::table('cars')
-        ->where('id', 12)
-        ->update(['car_picture' => 'front/img/cars/nissan-ultima-2022.jpg']);
+        // DB::table('cars')
+        // ->where('id', 12)
+        // ->update(['car_picture' => 'front/img/cars/nissan-ultima-2022.jpg']);
 
 
-        DB::table('cars')
-        ->where('id', 13)
-        ->update(['car_picture' => 'front/img/cars/elentra2022.jpg']);
+        // DB::table('cars')
+        // ->where('id', 13)
+        // ->update(['car_picture' => 'front/img/cars/elentra2022.jpg']);
 
 
-        DB::table('cars')
-        ->where('id', 14)
-        ->update(['car_picture' => 'front/img/cars/mercedes s500.png']);
+        // DB::table('cars')
+        // ->where('id', 14)
+        // ->update(['car_picture' => 'front/img/cars/mercedes s500.png']);
 
 
-        DB::table('cars')
-        ->where('id', 16)
-        ->update(['car_picture' => 'front/img/cars/landroverv4.jpg']);
+        // DB::table('cars')
+        // ->where('id', 16)
+        // ->update(['car_picture' => 'front/img/cars/landroverv4.jpg']);
 
-        DB::table('cars')
-        ->where('id', 17)
-        ->update(['car_picture' => 'front/img/cars/landroverv6.jpg']);
+        // DB::table('cars')
+        // ->where('id', 17)
+        // ->update(['car_picture' => 'front/img/cars/landroverv6.jpg']);
 
-        DB::table('cars')
-        ->where('id', 18)
-        ->update(['car_picture' => 'front/img/cars/rangrover.jpg']);
-
-
-        DB::table('cars')
-        ->where('id', 19)
-        ->update(['car_picture' => 'front/img/cars/nissanversa2021.jpg']);
+        // DB::table('cars')
+        // ->where('id', 18)
+        // ->update(['car_picture' => 'front/img/cars/rangrover.jpg']);
 
 
-        DB::table('cars')
-        ->where('id', 20)
-        ->update(['car_picture' => 'front/img/cars/2023-Malibu-Cover-watermark.jpg']);
+        // DB::table('cars')
+        // ->where('id', 19)
+        // ->update(['car_picture' => 'front/img/cars/nissanversa2021.jpg']);
 
 
-        DB::table('cars')
-        ->where('id', 10)
-        ->delete();
+        // DB::table('cars')
+        // ->where('id', 20)
+        // ->update(['car_picture' => 'front/img/cars/2023-Malibu-Cover-watermark.jpg']);
 
-        DB::table('cars')
-        ->where('id', 15)
-        ->delete();
+
+        // DB::table('cars')
+        // ->where('id', 10)
+        // ->delete();
+
+        // DB::table('cars')
+        // ->where('id', 15)
+        // ->delete();
     }
 
 
