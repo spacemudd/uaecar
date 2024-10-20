ffbac4b7c28cd0217a1b4d846f4482374669f2bf
Abdelrahman Yousef <abdelrahman.yousef@hadaf-hq.com>
2024-10-20 09:32:06 +0300

    update car images

--- database/seeders/UpdateCarPictureSeeder.php
+++ database/seeders/UpdateCarPictureSeeder.php
@@ -13,9 +13,66 @@ class UpdateCarPictureSeeder extends Seeder
     public function run(): void
     {
         DB::table('cars')
-        ->where('id', 2)
-        ->update(['car_picture' => 'front/img/cars/infitix.jpg']);
+        ->where('id', 4)
+        ->update(['car_picture' => 'front/img/cars/nissansunny2020.jpg']);
+
+
+        DB::table('cars')
+        ->where('id', 6)
+        ->update(['car_picture' => 'front/img/cars/hyundai-santa-fe-2023-exterior-oem-01.jpg']);
+
+        
+        DB::table('cars')
+        ->where('id', 9)
+        ->update(['car_picture' => 'front/img/cars/kiacerato2020.jpg']);
+
+
+        DB::table('cars')
+        ->where('id', 12)
+        ->update(['car_picture' => 'front/img/cars/nissan-ultima-2022.jpg']);
+
+
+        DB::table('cars')
+        ->where('id', 13)
+        ->update(['car_picture' => 'front/img/cars/elentra2022.jpg']);
+
+
+        DB::table('cars')
+        ->where('id', 14)
+        ->update(['car_picture' => 'front/img/cars/mercedes s500.png']);
+
+
+        DB::table('cars')
+        ->where('id', 16)
+        ->update(['car_picture' => 'front/img/cars/landroverv4.jpg']);
+
+        DB::table('cars')
+        ->where('id', 17)
+        ->update(['car_picture' => 'front/img/cars/landroverv6.jpg']);
+
+        DB::table('cars')
+        ->where('id', 18)
+        ->update(['car_picture' => 'front/img/cars/rangrover.jpg']);
+
+
+        DB::table('cars')
+        ->where('id', 19)
+        ->update(['car_picture' => 'front/img/cars/nissanversa2021.jpg']);
+
+
+        DB::table('cars')
+        ->where('id', 20)
+        ->update(['car_picture' => 'front/img/cars/2023-Malibu-Cover-watermark.jpg']);
+
+
+        DB::table('cars')
+        ->where('id', 10)
+        ->delete();
+
+        DB::table('cars')
+        ->where('id', 15)
+        ->delete();
     }
 
-    
+
 }
