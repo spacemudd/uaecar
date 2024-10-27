ebce92e370aa37220c0576f3e6d2aa7d4c82c705
Abdelrahman Yousef <abdelrahman.yousef@hadaf-hq.com>
2024-10-27 13:57:16 +0300

    edit car gallary

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
         //]);
 
         // $this->call(ArrivedSoonCar::class);
-        // $this->call(CarGallerySeeder::class);
+        $this->call(CarGallerySeeder::class);
 
         // $this->call(CarGallerySeeder::class);
         // $this->call(CarImageSeeder::class);
