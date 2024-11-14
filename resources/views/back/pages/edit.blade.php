9ae01206b22b901297ffcd47947d0a08579d6e03
Abdelrahman Yousef <abdelrahman.yousef@hadaf-hq.com>
2024-11-14 11:50:01 +0300

    update categories list v2

--- resources/views/back/pages/edit.blade.php
+++ resources/views/back/pages/edit.blade.php
@@ -89,8 +89,8 @@
                                         <option value="Luxury" {{ old('categories', $categoryValue) == 'Luxury' ? 'selected' : '' }}>Luxury</option>
                                         <option value="Premium" {{ old('categories', $categoryValue) == 'Premium' ? 'selected' : '' }}>Premium</option>
                                         <option value="Economy" {{ old('categories', $categoryValue) == 'Economy' ? 'selected' : '' }}>Economy</option>
-                                        <option value="Economy" {{ old('categories', $categoryValue) == 'Sports and Exotics' ? 'selected' : '' }}>Sports and Exotics</option>
-                                        <option value="Economy" {{ old('categories', $categoryValue) == 'Vans and Buses' ? 'selected' : '' }}>Vans and Buses</option>
+                                        <option value="Sports and Exotics" {{ old('categories', $categoryValue) == 'Sports and Exotics' ? 'selected' : '' }}>Sports and Exotics</option>
+                                        <option value="Vans and Buses" {{ old('categories', $categoryValue) == 'Vans and Buses' ? 'selected' : '' }}>Vans and Buses</option>
 
                                     </select>
                                 </div>
