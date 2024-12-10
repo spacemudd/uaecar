8dd5953c4e17a5ffde4e70a275d6b6ba7ccd5577
Abdelrahman Yousef <abdelrahman.yousef@hadaf-hq.com>
2024-12-10 15:26:03 +0300

    aas

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -157,7 +157,7 @@ class FormController extends Controller
             $updatedEconomyCars = array_filter($economyCars, fn($car) => in_array($car['plate_number'], $apiPlateNumbers));
             $carWithLowestEconomyPrice = array_reduce($updatedEconomyCars, fn($lowestCar, $car) => ($lowestCar === null || $car['price_daily'] < $lowestCar['price_daily']) ? $car : $lowestCar);
     
-            dd($carWithLowestEconomyPrice);
+            dd($carWithLowestMidRangePrice);
     
             $carImage = $request->input('car_picture');
             session(['car_picture' => $carImage]);
