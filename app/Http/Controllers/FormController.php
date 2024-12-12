4670adb34f61ac4332d693dd0421feb17100d5b8
Abdelrahman Yousef <abdelrahman.yousef@hadaf-hq.com>
2024-12-12 14:14:18 +0300

    ert

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -124,7 +124,7 @@ class FormController extends Controller
         $carImage = $request->input('car_picture');
         $token = Cache::get('node_api_token') ?? $this->authenticate();
 
-        dd($car);
+        dd($plateNumber);
     
         // إذا كانت السيارة غير موجودة أو غير متوفرة
         if (!$car || $car['status'] !== 'Available') {
