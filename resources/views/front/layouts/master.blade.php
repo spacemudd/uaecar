c17884f3b07960734c15c0811ec3d6c1ce635419
Abdelrahman Yousef <abdelrahman.yousef@hadaf-hq.com>
2024-12-15 10:06:48 +0300

    ert

--- resources/views/front/layouts/master.blade.php
+++ resources/views/front/layouts/master.blade.php
@@ -27,7 +27,7 @@
     
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/css/bootstrap.min.css" />
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" />
     
 </head>
 <body>
