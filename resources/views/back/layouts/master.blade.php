d0fafd8a39b87ad4c6515489360bdb2622c95be1
Abdelrahman Yousef <abdelrahman.yousef@hadaf-hq.com>
2024-12-15 09:28:12 +0300

    update on dashboard

--- resources/views/back/layouts/master.blade.php
+++ resources/views/back/layouts/master.blade.php
@@ -386,7 +386,7 @@
                     </div>
                     <span class="profile-username">
                       <span class="op-7">Hi,</span>
-                      <span class="fw-bold">Admin</span>
+                      <span class="fw-bold">{{ auth()->user()->name }}</span>
                     </span>
                   </a>
                   <ul class="dropdown-menu dropdown-user animated fadeIn">
