e9ff26a78b24ab939d8675001b83333bd56e5481
Abdelrahman Yousef <abdelrahman.yousef@hadaf-hq.com>
2024-12-15 16:02:55 +0300

    updaye form

--- resources/views/front/pages/index.blade.php
+++ resources/views/front/pages/index.blade.php
@@ -335,7 +335,7 @@
                                         @csrf
                                         <input type="hidden" name="car_id" value="{{ $car->id }}">
                                         <img src="{{ asset('storage/' . $car->car_picture) }}" alt="Car Picture" />
-<input type="hidden" name="car_picture" value="{{ asset('storage/' . $car->car_picture) }}">
+                                        <input type="hidden" name="car_picture" value="{{ asset('storage/' . $car->car_picture) }}">
                                         <div class="row">
                                             <div class="col-12">
                                                 <div class="alert alert-success contact__msg" style="display: none" role="alert"> Your message was sent successfully. </div>
@@ -501,8 +501,8 @@
                                             <form method="post" action="{{ route('form.submit') }}" class="form1 contact__form clearfix" id="bookingForm">
                                                 @csrf
                                                 <input type="hidden" name="car_id" value="{{ $car->id }}">
-                                                <input type="hidden" name="car_picture" value="{{ asset('storage/' . $car->car_picture) }}"> <!-- Hidden input for the car picture -->
-                                                <div class="row">
+                                                <img src="{{ asset('storage/' . $car->car_picture) }}" alt="Car Picture" />
+                                                <input type="hidden" name="car_picture" value="{{ asset('storage/' . $car->car_picture) }}">                                                <div class="row">
                                                     <div class="col-12">
                                                         <div class="alert alert-success contact__msg" style="display: none" role="alert"> Your message was sent successfully. </div>
                                                     </div>
@@ -666,8 +666,8 @@
                                 <form method="post" action="{{ route('form.submit') }}" class="form1 contact__form clearfix" id="bookingForm">
                                     @csrf
                                     <input type="hidden" name="car_id" value="{{ $car->id }}">
-                                    <input type="hidden" name="car_picture" value="{{ asset('storage/' . $car->car_picture) }}"> <!-- Hidden input for the car picture -->
-                                    <div class="row">
+                                    <img src="{{ asset('storage/' . $car->car_picture) }}" alt="Car Picture" />
+                                    <input type="hidden" name="car_picture" value="{{ asset('storage/' . $car->car_picture) }}">                                    <div class="row">
                                         <div class="col-12">
                                             <div class="alert alert-success contact__msg" style="display: none" role="alert"> Your message was sent successfully. </div>
                                         </div>
@@ -831,8 +831,8 @@
                                 <form method="post" action="{{ route('form.submit') }}" class="form1 contact__form clearfix" id="bookingForm">
                                     @csrf
                                     <input type="hidden" name="car_id" value="{{ $car->id }}">
-                                    <input type="hidden" name="car_picture" value="{{ asset('storage/' . $car->car_picture) }}"> <!-- Hidden input for the car picture -->
-                                    <div class="row">
+                                    <img src="{{ asset('storage/' . $car->car_picture) }}" alt="Car Picture" />
+                                    <input type="hidden" name="car_picture" value="{{ asset('storage/' . $car->car_picture) }}">                                    <div class="row">
                                         <div class="col-12">
                                             <div class="alert alert-success contact__msg" style="display: none" role="alert"> Your message was sent successfully. </div>
                                         </div>
@@ -995,8 +995,8 @@
                                 <form method="post" action="{{ route('form.submit') }}" class="form1 contact__form clearfix" id="bookingForm">
                                     @csrf
                                     <input type="hidden" name="car_id" value="{{ $car->id }}">
-                                    <input type="hidden" name="car_picture" value="{{ asset('storage/' . $car->car_picture) }}"> <!-- Hidden input for the car picture -->
-                                    <div class="row">
+                                    <img src="{{ asset('storage/' . $car->car_picture) }}" alt="Car Picture" />
+                                    <input type="hidden" name="car_picture" value="{{ asset('storage/' . $car->car_picture) }}">                                    <div class="row">
                                         <div class="col-12">
                                             <div class="alert alert-success contact__msg" style="display: none" role="alert"> Your message was sent successfully. </div>
                                         </div>
