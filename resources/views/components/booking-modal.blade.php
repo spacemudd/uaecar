e023e71dc42ef7dadfa085f403f82d5ad19f5715
Abdelrahman Yousef <abdelrahman.yousef@hadaf-hq.com>
2025-02-06 14:58:27 +0300

    car modal form

--- resources/views/components/booking-modal.blade.php
+++ resources/views/components/booking-modal.blade.php
@@ -58,7 +58,7 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="bookingModalLabel{{ $car->id }}">Booking Form for {{ $car->car_name }}</h5>
+                <h5 class="modal-title" id="bookingModalLabel{{ $car->id }}">Booking Form for {{ $car->car_name . $car->model }}</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
