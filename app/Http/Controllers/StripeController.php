4633176ac8ab0df7124b2b81e10c2e914115e243
Abdelrahman Yousef <abdelrahman.yousef@hadaf-hq.com>
2025-02-20 10:17:38 +0300

    update

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -7,12 +7,7 @@ use Stripe\StripeClient;
 use App\Models\Car;
 use App\Models\Invoice;
 use Illuminate\Support\Facades\Http;
-use App\Mail\InvoiceEmail;
-use Illuminate\Support\Facades\Mail;
-use Barryvdh\Snappy\Facades\SnappyPdf as PDF;
-use Illuminate\Support\Facades\Log;
 use App\Jobs\SendInvoiceEmailJob;
-
 
 class StripeController extends Controller
 {
@@ -43,55 +38,16 @@ class StripeController extends Controller
         ]);
 
         $car = Car::find($request->car_id);
-
-        if(session('rate_daily') >= 348){
-            $invoice = Invoice::create([
-                'customer_name' => $request->customer_name,
-                'customer_email' => $request->customer_email,
-                'customer_phone' => $request->customer_phone,
-                'city' => $request->customer_city,
-                'pickup_date' => session('pickup_date'),
-                'return_date' => session('return_date'),
-                'creation_date' => now(),
-                'description' => "This is a new Car",
-                'car_daily_price' => session('rate_daily'),
-                'total_days' => \Carbon\Carbon::parse(session('pickup_date'))
-                    ->diffInDays(\Carbon\Carbon::parse(session('return_date'))),
-                'total_amount' => $request->total,
-                'tax' => 0,
-                'status' => 'Payment Received',
-            ]);
-        }else{
-            $invoice = Invoice::create([
-                'customer_name' => $request->customer_name,
-                'customer_email' => $request->customer_email,
-                'customer_phone' => $request->customer_phone,
-                'city' => $request->customer_city,
-                'pickup_date' => session('pickup_date'),
-                'return_date' => session('return_date'),
-                'creation_date' => now(),
-                'description' => session('car_name'),
-                'car_daily_price' => session('rate_daily'),
-                'total_days' => \Carbon\Carbon::parse(session('pickup_date'))
-                    ->diffInDays(\Carbon\Carbon::parse(session('return_date'))),
-                'total_amount' => $request->total,
-                'tax' => 1000,
-                'status' => 'Payment Received',
-            ]);
-        }
-        
-
         
         $deposite_amount = 0;
-        if ($car->price_daily <= 349){
+        if ($car->price_daily <= 349) {
             $deposite_amount = 1000;
-        }else{
-            $deposite_amount = 0;
         }
+
         // قم بإنشاء جلسة الدفع في Stripe
         $session = $this->stripe->checkout->sessions->create([
             'mode' => 'payment',
-            'success_url' => route('successview', ['session_id' => '{CHECKOUT_SESSION_ID}', 'invoice_id' => $invoice->id]),
+            'success_url' => route('successview', ['session_id' => '{CHECKOUT_SESSION_ID}']),
             'line_items' => [
                 [
                     'price_data' => [
@@ -99,15 +55,13 @@ class StripeController extends Controller
                         'product_data' => [
                             'name' => $car->car_name . ' ' . $car->model . ' ' . $car->year,
                             'description' => 'Pickup Date: ' . $request->pickup_date,
-                            'description' => 'Return Date: ' . $request->return_date,
-
-                            'images' => [asset('storage/' . $car->car_picture)],                        
+                            'images' => [asset('storage/' . $car->car_picture)],
                         ],
                         'unit_amount' => $request->total * 100, // المبلغ الأصلي
                     ],
                     'quantity' => 1,
                 ],
-                // إضاeفة مبلغ التأمين
+                // إضافة مبلغ التأمين
                 [
                     'price_data' => [
                         'currency' => 'AED',
@@ -118,7 +72,6 @@ class StripeController extends Controller
                     ],
                     'quantity' => 1,
                 ],
-              
             ],
             'metadata' => [
                 'pickup_date' => $request->pickup_date,
@@ -131,7 +84,8 @@ class StripeController extends Controller
             ],
         ]);
         
-        session(['user_invoices' => $invoice->id]);
+        // تخزين معرف الجلسة في الجلسة
+        session(['checkout_session_id' => $session->id]);
 
         return redirect($session->url);
     }
@@ -151,56 +105,56 @@ class StripeController extends Controller
         return null;
     }
 
-
-
-
     public function successView(Request $request)
     {
-        $invoiceId = session('user_invoices');
-        
-        if (!$invoiceId) {
-            return redirect()->route('payment.cancel');
+        $sessionId = $request->get('session_id');
+
+        // استرداد معلومات الجلسة من Stripe
+        $session = $this->stripe->checkout->sessions->retrieve($sessionId);
+
+        // التحقق من نجاح الدفع
+        if ($session->payment_status === 'paid') {
+            // الآن يمكنك إنشاء الفاتورة
+            if(session('rate_daily') >= 348){
+                $invoice = Invoice::create([
+                    'customer_name' => $request->customer_name,
+                    'customer_email' => $request->customer_email,
+                    'customer_phone' => $request->customer_phone,
+                    'city' => $request->customer_city,
+                    'pickup_date' => session('pickup_date'),
+                    'return_date' => session('return_date'),
+                    'creation_date' => now(),
+                    'description' => "This is a new Car",
+                    'car_daily_price' => session('rate_daily'),
+                    'total_days' => \Carbon\Carbon::parse(session('pickup_date'))
+                        ->diffInDays(\Carbon\Carbon::parse(session('return_date'))),
+                    'total_amount' => $request->total,
+                    'tax' => 0,
+                    'status' => 'Payment Received',
+                ]);
+            }else{
+                $invoice = Invoice::create([
+                    'customer_name' => $request->customer_name,
+                    'customer_email' => $request->customer_email,
+                    'customer_phone' => $request->customer_phone,
+                    'city' => $request->customer_city,
+                    'pickup_date' => session('pickup_date'),
+                    'return_date' => session('return_date'),
+                    'creation_date' => now(),
+                    'description' => session('car_name'),
+                    'car_daily_price' => session('rate_daily'),
+                    'total_days' => \Carbon\Carbon::parse(session('pickup_date'))
+                        ->diffInDays(\Carbon\Carbon::parse(session('return_date'))),
+                    'total_amount' => $request->total,
+                    'tax' => 1000,
+                    'status' => 'Payment Received',
+                ]);
+            }
+
+            // إرسال البريد الإلكتروني مع الفاتورة
+            SendInvoiceEmailJob::dispatch($invoice);
         }
-        
-        $invoice = Invoice::find($invoiceId);
-        if (!$invoice) {
-            return redirect()->route('payment.cancel');
-        }
-    
-        $token = $this->getAuthToken();
-        if (!$token) {
-            return redirect()->route('payment.cancel')->with('error', 'فشل في الحصول على توكن المصادقة');
-        }
-    
-        $carId = Car::find(session('new_id'));
-        $carName = $carId->car_name;
-        $carModel = $carId->model;
-        $carYear = $carId->year;
-        $carPlateNumber = $carId->plate_number;
-        $pickupDate = \Carbon\Carbon::parse(session('pickup_date'))->format('Y-m-d H:i:s');
-        $returnDate = \Carbon\Carbon::parse(session('return_date'))->format('Y-m-d H:i:s');
-    
-        $apiUrl = 'https://luxuria.crs.ae/api/v1/reservations';
-        $apiData = [
-            'customer_name' => $invoice->customer_name,
-            'customer_mobile' => $invoice->customer_phone,
-            'customer_email' => $invoice->customer_email,
-            'customer_nationality' => 'ARE',
-            'pickup_location' => '71',
-            'return_location' => '71',
-            'pickup_date' => $pickupDate,
-            'return_date' => $returnDate,
-            'vehicle_hint' => $carName . ' ' . $carModel . ' ' . $carYear . ' ' . $carPlateNumber,
-            'rate_daily' => session('rate_daily'),
-            'status' => 'confirmed',
-        ];
-    
-        $response = Http::withHeaders([
-            'Authorization' => 'Bearer ' . $token,
-        ])->post($apiUrl, $apiData);
-    
-        SendInvoiceEmailJob::dispatch($invoice);      
-        return view('front.pages.successView', compact('invoiceId'));
+
+        return view('front.pages.successView', compact('invoice'));
     }
-    
 }
