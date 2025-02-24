05afabaf34b851084ac91a28084ecca23502ced4
Abdelrahman Yousef <abdelrahman.yousef@hadaf-hq.com>
2025-02-24 15:21:41 +0300

    update users api

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -1,9 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
 
 class UserController extends Controller
 {
