5cc6bb98934d69db5f27b781f9868d95b5966d6a
Abdelrahman Yousef <abdelrahman.yousef@hadaf-hq.com>
2025-02-24 08:57:03 +0300

    update users

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'phone_number' => 'required|string|max:15',
+            'email_address' => 'required|string|email|max:255|unique:users',
+            'pickup_city' => 'required|string|max:255',
+        ]);
+
+        $user = User::create($request->all());
+
+        return response()->json($user, 201);
+    }
+}
