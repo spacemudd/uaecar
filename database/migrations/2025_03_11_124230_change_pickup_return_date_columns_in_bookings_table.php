9aa693d0390023b0128646ee2c451ab409f91012
Abdelrahman Yousef <abdelrahman.yousef@hadaf-hq.com>
2025-03-11 15:43:26 +0300

    ert

---
+++ database/migrations/2025_03_11_124230_change_pickup_return_date_columns_in_bookings_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangePickupReturnDateColumnsInBookingsTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->dateTime('pickup_date')->change();
+            $table->dateTime('return_date')->change();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->date('pickup_date')->change();
+            $table->date('return_date')->change();
+        });
+    }
+}
